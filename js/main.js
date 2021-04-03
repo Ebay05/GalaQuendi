625ed0687eb43f860150ed97acd0a20a472d08e6
Ebay05 <ebay11990011@gmail.com>
2021-04-03 17:30:23 +0200

    Login Site (Mobile Menu Completed)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const topBar = document.querySelector(".top-bar");
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
+    navLinks.classList.toggle("fixed");
+    navLinks.classList.toggle("links-open");
     navLinks.classList.toggle("nav-links");
+    topBar.classList.toggle("top-bar-open")
 })
