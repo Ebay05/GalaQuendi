1515841152d4b3f77df35b58529521fb6bbbeb0f
Ebay05 <ebay11990011@gmail.com>
2021-04-03 15:17:22 +0200

    Login Site (Mobile Menu)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
-const mobile = window.matchMedia('(max-width: 1024px)')
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-if(mobile.matches) {
-    const hamburgerBtn = document.querySelector("#hamburger-menu-btn");
-    const mobileMenu = document.querySelector("#hamburger-menu-container");
-
-    mobileMenu.classList.add("hamburger-menu-none");
-
-    hamburgerBtn.addEventListener("click", function(){
-        mobileMenu.classList.toggle("hamburger-menu-none");
-        mobileMenu.classList.toggle("hamburger-menu-container");
-    });
-} else{
-    const hamburgerMenu = document.querySelector("#hamburger-menu-container");
-    hamburgerMenu.classList.add("hamburger-menu-none");
-}
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    navLinks.classList.toggle("nav-links");
+})
